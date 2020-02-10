96aa8baacdf511682b5cf1bf0e3fa060f717cd19
dan_anddy <taorus.warior@gmail.com>
2020-02-10 18:02:51 +0200

    need some fixes, main program is done already. yet to be fixed

--- AndriychukTest/src/main/java/Controller.java
+++ AndriychukTest/src/main/java/Controller.java
@@ -9,33 +9,34 @@ public class Controller {
     private int secretNumber;
     private ArrayList statArray = new ArrayList();
 
-    public Controller(Model model, View view){
+    private Controller(Model model, View view){
         this.model = model;
         this.view = view;
     }
 
-    public void readUserData(){
+    private void readUserData(){
         Scanner scanner = new Scanner(System.in);
 
         secretNumber = model.setRandValueOfSecretNumber();
         System.out.println(secretNumber);
         int userSecretNumber = getUserNumber(scanner);
 
-       view.printWinMassege(view.OUTPUT_WIN_MESSAGE, userSecretNumber, View.OUTPUT_STATISTIC, statArray);
+       view.printWinMassege(View.OUTPUT_WIN_MESSAGE, userSecretNumber, View.OUTPUT_STATISTIC, statArray);
     }
 
-    public int getUserNumber(Scanner scan){
+    private int getUserNumber(Scanner scan){
 
-        view.printMessage(view.ENTER_FIRST_NUMBER);
+        view.printMessage(View.ENTER_FIRST_NUMBER);
         int userValue = scan.nextInt();
 
         while(!checkGuess(userValue)){
             if(!checkBounds(userValue)){
-                view.printMessage(view.WRONG_INPUT_OUT_OF_BOUNDARIES);
+                view.printMessage(View.WRONG_INPUT_OUT_OF_BOUNDARIES);
                 view.printBounds(model.getValOfBounds());
+                statArray.add(userValue);
             }
             else{
-                view.printMessage(view.OUTPUT_INFO_ON_BOUNDARIES);
+                view.printMessage(View.OUTPUT_INFO_ON_BOUNDARIES);
 
                 model.setBoundries(userValue);
 
@@ -49,12 +50,10 @@ public class Controller {
         return userValue;
     }
 
-    public boolean checkBounds(int userNumber){
-        if(!(model.checkBounds(userNumber))) return false;
-        else return true;
+    private boolean checkBounds(int userNumber){
+        return model.checkBounds(userNumber);
     }
-    public boolean checkGuess(int userNumber){
-        if((secretNumber)== userNumber) return true;
-        else return false;
+    private boolean checkGuess(int userNumber){
+        return (secretNumber) == userNumber;
     }
 }
